28338a4f2f214855b2725404e69593e9c321ad47
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:41:16 +0300

    add new phonetic mapping 508

--- src/main/java/data/Datasource.java
+++ src/main/java/data/Datasource.java
@@ -536,6 +536,7 @@ public class Datasource {
         phonetics.put(505,"505");
         phonetics.put(506,"506");
         phonetics.put(507,"507");
+        phonetics.put(508,"508");
 //HERE!
     }
 
