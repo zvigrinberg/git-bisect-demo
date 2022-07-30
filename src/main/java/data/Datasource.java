becff1a746b55754011c2d1174fdb1cf91c1d30f
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:41:20 +0300

    add new phonetic mapping 767

--- src/main/java/data/Datasource.java
+++ src/main/java/data/Datasource.java
@@ -795,6 +795,7 @@ public class Datasource {
         phonetics.put(764,"764");
         phonetics.put(765,"765");
         phonetics.put(766,"766");
+        phonetics.put(767,"767");
 //HERE!
     }
 
