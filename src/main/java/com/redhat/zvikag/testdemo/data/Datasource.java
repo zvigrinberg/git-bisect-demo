b7175ed6cb6cb4b546084b7ff38a6358b2c39457
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:33:27 +0300

    add new phonetic mapping 223

--- src/main/java/com/redhat/zvikag/testdemo/data/Datasource.java
+++ src/main/java/com/redhat/zvikag/testdemo/data/Datasource.java
@@ -252,6 +252,7 @@ public class Datasource {
         phonetics.put(220,"220");
         phonetics.put(221,"221");
         phonetics.put(222,"222");
+        phonetics.put(223,"223");
 //HERE!
     }
 
