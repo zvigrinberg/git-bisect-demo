47e596751c0cf42e2ee43f385bb31e501a76f43c
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:33:26 +0300

    add new phonetic mapping 175

--- src/main/java/com/redhat/zvikag/testdemo/data/Datasource.java
+++ src/main/java/com/redhat/zvikag/testdemo/data/Datasource.java
@@ -204,6 +204,7 @@ public class Datasource {
         phonetics.put(172,"172");
         phonetics.put(173,"173");
         phonetics.put(174,"174");
+        phonetics.put(175,"175");
 //HERE!
     }
 
