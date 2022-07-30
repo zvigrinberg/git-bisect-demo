3cb76f79693e9624cb3c24bbbcf07180305fa594
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:41:15 +0300

    add new phonetic mapping 405

--- src/main/java/data/Datasource.java
+++ src/main/java/data/Datasource.java
@@ -433,6 +433,7 @@ public class Datasource {
         phonetics.put(402,"402");
         phonetics.put(403,"403");
         phonetics.put(404,"404");
+        phonetics.put(405,"405");
 //HERE!
     }
 
