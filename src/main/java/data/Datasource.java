3d7f48a55bf831d6b6d96608a1c6c31ab81bcd0c
zvigrinberg <zgrinberg@gmail.com>
2022-07-31 02:41:20 +0300

    add new phonetic mapping 770

--- src/main/java/data/Datasource.java
+++ src/main/java/data/Datasource.java
@@ -798,6 +798,7 @@ public class Datasource {
         phonetics.put(767,"767");
         phonetics.put(768,"768");
         phonetics.put(769,"769");
+        phonetics.put(770,"770");
 //HERE!
     }
 
